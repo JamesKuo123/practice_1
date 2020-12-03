78cd3e76cce6cd4851e7dccc0b966a8d727fc917
JamesK <49828760+JamesKuo123@users.noreply.github.com>
2020-12-03 17:26:30 +0800

    update1203

---
+++ js/chunk-2d0afdef.61db7519.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0afdef"],{"0fbe":function(e,t,n){"use strict";n.r(t);var s=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("News")])])}],c=n("2877"),u={},l=Object(c["a"])(u,s,a,!1,null,null,null);t["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d0afdef.61db7519.js.map

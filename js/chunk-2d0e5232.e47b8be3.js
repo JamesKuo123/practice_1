67687d041e51a2070f558ce00cb6dbcda9e1c25f
JamesK <49828760+JamesKuo123@users.noreply.github.com>
2021-01-29 12:55:15 +0800

    20210129

---
+++ js/chunk-2d0e5232.e47b8be3.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e5232"],{"92cf":function(e,n,t){"use strict";t.r(n);var s=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"onlinestore"},[t("h1",[e._v("線上購物")]),t("router-view")],1)},c=[],l={},r=l,u=t("2877"),i=Object(u["a"])(r,s,c,!1,null,null,null);n["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d0e5232.e47b8be3.js.map
